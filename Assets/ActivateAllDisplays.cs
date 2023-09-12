1c8ec55daa9d330b37386d61ee81fed8380ac1b2
KittyC513 <82276776+KittyC513@users.noreply.github.com>
2023-09-12 13:14:03 -0400

    Multi-display setup

--- Assets/ActivateAllDisplays.cs
+++ Assets/ActivateAllDisplays.cs
@@ -8,11 +8,12 @@ public class ActivateAllDisplays : MonoBehaviour
         Debug.Log("displays connected: " + Display.displays.Length);
         // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
         // Check if additional displays are available and activate each.
-
         for (int i = 1; i < Display.displays.Length; i++)
         {
             Display.displays[i].Activate();
         }
+        Display.displays[1].Activate();
+        Display.displays[2].Activate();
     }
 
     void Update()
