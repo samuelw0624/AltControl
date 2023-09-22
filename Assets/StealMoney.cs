c9486c71f85728ddeebb2e5283d0495c2d8b503b
KittyC513 <82276776+KittyC513@users.noreply.github.com>
2023-09-22 10:22:27 -0400

    Merge issue

--- Assets/StealMoney.cs
+++ Assets/StealMoney.cs
@@ -16,7 +16,7 @@ public class StealMoney : MonoBehaviour
         
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("PlayerCollider"))
         {
