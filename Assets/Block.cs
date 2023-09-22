a417ebca6daceba8d2dea88391b1a655ec32d921
KittyC513 <82276776+KittyC513@users.noreply.github.com>
2023-09-22 11:23:17 -0400

    Implement Enemy Kite

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    public GameObject block;
+    int i;
+    Vector3 blockPos;
+    Vector3 myPos;
+
+    Vector3 dir;
+
+    bool isCollided;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +21,43 @@ public class Block : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateDirection();
+
+    }
+
+
+    #region Detect Direction Between Player and blocks
+    void CalculateDirection()
+    {
+        blockPos = block.transform.position;
+        myPos = this.transform.position;
+
+        dir = blockPos.normalized - myPos.normalized;
+        //float distance = Vector3.Distance(blockPos, myPos);
+
+        Debug.Log(dir);
+    }
+
+    void DetectDirection()
+    {
         
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "block")
+        {
+            isCollided = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "block")
+        {
+            isCollided = false;
+        }
+    }
 
 
 
